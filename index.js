93128d04247de41404f57c5bc1c352cb2f78ce20
Joel Pierre-Powell <jpp@jppdesigns.co.uk>
2022-06-22 20:42:31 +0100

    chore: initial push

---
+++ index.js
@@ -0,0 +1,34 @@
+const crypto = require('crypto');
+
+const settings = {key: null};
+
+const queryParamsToString = (params = []) => {
+  return params.reduce((acc, {name, value}) => {
+    if (!name || !value) {
+      return acc;
+    }
+    return `${acc || ''}${acc ? '&' : ''}${name}=${value}`
+  }, '');
+};
+
+function createSignature(queryParams) {
+  return crypto.createHmac('sha256', settings.key).update(queryParams, 'utf8').digest('base64');
+}
+
+module.exports = {
+  templateTags: [{
+    name: 'unleashedapiauthsig',
+    displayName: 'Unleashed Api Signature',
+    args: [{displayName: 'Key', type: 'string', placeholder: 'Unleashed API Key'}, {
+      displayName: 'Query Params',
+      type: 'string',
+      placeholder: 'JSON stringified array of name=value pairs: [{"name": "foo", "value": "bar"}]'
+    },],
+    run: (context, key = '', queryParams = '') => {
+      settings.key = key
+      return createSignature(queryParamsToString(queryParams ? JSON.parse(queryParams) : []));
+    }
+  }], requestHooks: [context => {
+    context.request.setHeader('api-auth-signature', createSignature(queryParamsToString(context.request.getParameters() || [])));
+  }]
+};
