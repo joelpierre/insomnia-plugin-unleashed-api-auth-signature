89ea41c2525158cbbfcbe4e7250f9155a1114a40
Joel Pierre-Powell <jpp@jppdesigns.co.uk>
2022-07-15 15:51:04 +0100

    chore: add eslint and prettier

--- index.js
+++ index.js
@@ -1,34 +1,72 @@
-const crypto = require('crypto');
-
-const settings = {key: null};
-
-const queryParamsToString = (params = []) => {
-  return params.reduce((acc, {name, value}) => {
-    if (!name || !value) {
-      return acc;
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
     }
-    return `${acc || ''}${acc ? '&' : ''}${name}=${value}`
-  }, '');
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
-
-function createSignature(queryParams) {
-  return crypto.createHmac('sha256', settings.key).update(queryParams, 'utf8').digest('base64');
-}
-
-module.exports = {
-  templateTags: [{
-    name: 'unleashedapiauthsig',
-    displayName: 'Unleashed Api Signature',
-    args: [{displayName: 'Key', type: 'string', placeholder: 'Unleashed API Key'}, {
-      displayName: 'Query Params',
-      type: 'string',
-      placeholder: 'JSON stringified array of name=value pairs: [{"name": "foo", "value": "bar"}]'
-    },],
-    run: (context, key = '', queryParams = '') => {
-      settings.key = key
-      return createSignature(queryParamsToString(queryParams ? JSON.parse(queryParams) : []));
+define("index", ["require", "exports", "crypto"], function (require, exports, crypto) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    crypto = __importStar(crypto);
+    const settings = { key: '' };
+    const queryParamsToString = (params = []) => {
+        return params.reduce((acc, { name, value }) => {
+            if (!name || !value) {
+                return acc;
+            }
+            return `${acc || ''}${acc ? '&' : ''}${name}=${value}`;
+        }, '');
+    };
+    function createSignature(queryParams) {
+        return crypto
+            .createHmac('sha256', settings.key)
+            .update(queryParams, 'utf8')
+            .digest('base64');
     }
-  }], requestHooks: [context => {
-    context.request.setHeader('api-auth-signature', createSignature(queryParamsToString(context.request.getParameters() || [])));
-  }]
-};
+    module.exports = {
+        templateTags: [
+            {
+                name: 'unleashedapiauthsig',
+                displayName: 'Unleashed Api Signature',
+                args: [
+                    {
+                        displayName: 'Key',
+                        type: 'string',
+                        placeholder: 'Unleashed API Key',
+                    },
+                    {
+                        displayName: 'Query Params',
+                        type: 'string',
+                        placeholder: 'JSON stringified array of name=value pairs: [{"name": "foo", "value": "bar"}]',
+                    },
+                ],
+                run: (context, key = '', queryParams = '') => {
+                    settings.key = key;
+                    return createSignature(queryParamsToString(queryParams ? JSON.parse(queryParams) : []));
+                },
+            },
+        ],
+        requestHooks: [
+            (context) => {
+                context.request.setHeader('api-auth-signature', createSignature(queryParamsToString(context.request.getParameters() || [])));
+            },
+        ],
+    };
+});
